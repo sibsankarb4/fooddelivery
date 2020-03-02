e1048afa89ab24f42b23af6bffbaf29d396f77e3
Sibsankar Bera <sibsankarb4@gmail.com>
2020-03-02 13:14:01 +0530

    Update FoodDeliveryApplication.java

--- src/main/java/com/ibm/food/FoodDeliveryApplication.java
+++ src/main/java/com/ibm/food/FoodDeliveryApplication.java
@@ -2,10 +2,19 @@ package com.ibm.food;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class FoodDeliveryApplication {
+public class FoodDeliveryApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(FoodDeliveryApplication.class, args);
 	}
+	
+ 	@Override
+        protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+			return builder.sources(SpringbootOpenshiftJBossEAPExampleApplication.class);
+		}
+
 }
